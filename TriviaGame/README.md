9e776e0cf77edb44498b1fca60fbb798f766c2e0
lilalekat <lilalekat@yahoo.com>
2019-08-20 21:13:36 -0400

    Adding Files

--- TriviaGame/README.md
+++ TriviaGame/README.md
@@ -1,1 +1,21 @@
 # TriviaGame
+
+How to Play!
+
+Welcome to the 90s TV Show Triva Game!
+
+You have 45 seconds to answer 10 multiple-choice, trivia questions that are related to popular 90s TV Shows! Once you are finished selecting your answers, please then press the 'Submit' button and you will then see a tally of how many questions you answered correctly, those you answered incorrectly, and those that you were so bombed out on that you couldn't even answer! 🤪 The more that you answer correctly, the higher your score! 
+
+Good Luck and play as many times as you would like!🙌
+
+Game Linked Below!
+
+https://lilalekat.github.io/Trivia-Game/
+
+This Game Was Built Using:
+
+HTML5
+CSS
+Bootstrap
+Javascript
+JQuery
---
+++ TriviaGame/assets/javascript/app.js
@@ -0,0 +1,198 @@
+
+
+//Begin of Start Quiz on Click of the Start Button//
+$("#startQuizButton").on("click", function () {
+    //Removes Start Button After Quiz Has Began//
+    $("#startQuizButton").remove();
+    // //Declaration of Variables for Timer, If/Else, ForLoop
+    var time = 60
+    var x = setInterval(function () {
+        time = time - 1;
+        $("#timer").text(time);
+        if (time === 0) {
+            clearInterval(x)
+            time = 60
+        }
+    }, 1000);
+    for (var i = 0; i < triviaQuestions.length; i++) {
+        var questionP = $("<p>");
+        questionP.text(triviaQuestions[i].question);
+        $("#quizQuestions").append(questionP)
+        for (var j = 0; j < triviaQuestions[i].answerChoices.length; j++) {
+            $("#quizQuestions").append("<input type='radio' name='question-" + i + "' value='" + triviaQuestions[i].answerChoices[j] + "''>" + triviaQuestions[i].answerChoices[j]);
+
+        }
+
+    }
+
+});
+
+//Trivia Questins, Answers, and Correct Answers//
+var triviaQuestions = [
+    {
+        question: "Where was the Will Smith’s character “born and raised” in The Fresh Prince of Bel-Air?",
+        answerChoices: ["South Side of Chicago", "East Compton", "West Philadelphia"],
+        correctAnswer: "West Philadelphia"
+    },
+    {
+        question: "What was the name of the pet monkey that Ross had during the early part of the Friends series?",
+        answerChoices: ["Maurice", "Marcel", "Martell"],
+        correctAnswer: "Marcel"
+    },
+    {
+        question: "What was the name of the main female lead in the TV show, Boy Meets World?",
+        answerChoices: ["Topanga", "Winny", "Blossom"],
+        correctAnswer: "Topanga"
+    },
+    {
+        question: "What was the quote that everyone seems to remember from Michelle Tanner in Full House?",
+        answerChoices: ["You got it, dude.", "Cut. It. Out", "How rude!"],
+        correctAnswer: "You got it, dude."
+    },
+    {
+        question: "What was the name of the name of the blonde love interest for Buffy in Buffy the Vampire Slayer?",
+        answerChoices: ["Angel", "Xander", "Spike"],
+        correctAnswer: "Spike"
+    },
+    {
+        question: "What was the candy that Kramer dropped over the side while watching a medical procedure in Seinfeld?",
+        answerChoices: ["Kit Kats", "Milk Duds", "Junior Mints"],
+        correctAnswer: "Junior Mints"
+    },
+    {
+        question: "What was the name of the school principle from Saved by the Bell?",
+        answerChoices: ["Mr. Belding", "Mr. Belvidere", "Mr. Burns"],
+        correctAnswer: "Mr. Belding"
+    },
+    {
+        question: "What was the name of Tim 'The Toolman' Taylor's handyman show from Home Improvement? ",
+        answerChoices: ["Tool Time", "Tool Rules", "Tool Tricks"],
+        correctAnswer: "Tool Time"
+    },
+    {
+        question: "What was the catchphrase that Steve Urkel was known for in the '90s sitcom, Family Matters?",
+        answerChoices: ["What does that mean?", "Did I do that?", "You know I can dig it!"],
+        correctAnswer: "Did I do that?"
+    },
+    {
+        question: "What is the name of the '90s sitcom that featured the 'Soup Nazi'?",
+        answerChoices: ["Full House", "Twin Peaks", "Seinfeld"],
+        correctAnswer: "Seinfeld"
+    },
+];
+//Countdown function that callsback the gameCountdown function, when timer reaches 0//
+var triviaGame ={
+    correct: 0,
+    incorrect: 0,
+    counter: 60,
+
+    gameCountdown: function() {
+        triviaGame.counter--;
+        $("#timer").html(triviaGame.counter);
+        if (triviaGame.counter === 0) {
+            console.log("No More Tick Tock");
+            triviaGame.done();
+        }
+    }
+
+}
+
+//triviaGame.submit function, which tracks/tallies correct/incorrect answers for each question//
+$.each($("input[name='question-0']:checked"), function () {
+    if ($('question-0').val === triviaQuestions[0].correctAnswer) {
+        triviaGame.correct++;
+    } 
+    else {
+    triviaGame.incorrect++;
+    }
+});
+$.each($("input[name='question-1']:checked"), function () {
+    if ($('question-1').val === triviaQuestions[1].correctAnswer) {
+        triviaGame.correct++;
+    } 
+    else {
+    triviaGame.incorrect++;
+    }
+});
+$.each($("input[name='question-2']:checked"), function () {
+    if ($('question-2').val === triviaQuestions[2].correctAnswer) {
+        triviaGame.correct++;
+    } 
+    else {
+    game.incorrect++;
+    }
+});
+$.each($("input[name='question-3']:checked"), function () {
+    if ($('question-3').val === triviaQuestions[3].correctAnswer) {
+        triviaGame.correct++;
+    } 
+    else {
+    triviaGame.incorrect++;
+    }
+});
+$.each($("input[name='question-4']:checked"), function () {
+    if ($('question-4').val === triviaQuestions[4].correctAnswer) {
+        triviaGame.correct++;
+    } 
+    else {
+    triviaGame.incorrect++;
+    }
+});
+$.each($("input[name='question-5']:checked"), function () {
+    if ($('question-5').val === triviaQuestions[5].correctAnswer) {
+        triviaGame.correct++;
+    } 
+    else {
+    triviaGame.incorrect++;
+    }
+});
+$.each($("input[name='question-6']:checked"), function () {
+    if ($('question-6').val === triviaQuestions[6].correctAnswer) {
+        triviaGame.correct++;
+    } 
+    else {
+    triviaGame.incorrect++;
+    }
+});
+$.each($("input[name='question-7']:checked"), function () {
+    if ($('question-7').val === triviaQuestions[7].correctAnswer) {
+        triviaGame.correct++;
+    } 
+    else {
+    triviaGame.incorrect++;
+    }
+});
+$.each($("input[name='question-8']:checked"), function () {
+    if ($('question-8').val === triviaQuestions[8].correctAnswer) {
+        triviaGame.correct++;
+    } 
+    else {
+    triviaGame.incorrect++;
+    }
+});
+$.each($("input[name='question-9']:checked"), function () {
+    if ($('question-9').val === triviaQuestions[9].correctAnswer) {
+        triviaGame.correct++;
+    } 
+    else {
+    triviaGame.incorrect++;
+    }
+});
+
+submitQuiz();
+
+function submitQuiz () {
+    $("#submitQuizButton").on("click", function (event){
+    clearInterval("#timer");
+    $("#quizQuestions").remove();
+
+    ("#quizResults").html("<h2>Out of Time!</h2>");
+    ("#quizResults").append("<h3>Guessed Correctly: " + this.correct + "</h3>");
+    ("#quizResults").append("<h3>Horribly Guessed: " + this.incorrect + "</h3>");
+    ("#quizResults").append("<h3>Didn't Try At All: " + (triviaQuestions.length - (this.incorrect + this.correct)) +"</h3>");
+});
+};
+// 
+// $("#submitQuizButton").on("click", function (event) {
+    // event.preventDefault();
+
